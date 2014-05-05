c3c5789667540ba587c33f4aa4254b5b1f6a84df
duybkict <duytran.bkict@gmail.com>
2014-05-05 21:20:51 +0700

    Update

---
+++ constant.h
@@ -0,0 +1,16 @@
+#ifndef CONSTANT_H
+#define	CONSTANT_H
+
+#define DEBUG 1
+#define MAX_TOKEN_NUM 10
+#define MAX_PLAYER_POOL 260
+
+#define DELIMITER '|'
+#define SUCCESS 1
+#define ERROR 0
+
+#define MSG_LOGIN 0
+#define MSG_LOGOUT 1
+
+#endif	/* REQUESTER_H */
+
---
+++ requester.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "unp.h"
+#include "constant.h"
+
+#ifndef REQUESTER_H
+#define	REQUESTER_H
+
+int request_login(int , char *);
+
+#endif	/* REQUESTER_H */
+
